09f709fb9c8a2ab369a061a4d50dce2d8f9e1cc3
Daniel Jamrozik <djamro2@illinois.edu>
2015-11-07 18:42:48 -0600

    move to single page structure

--- client/app/controllers/IndexCtrl.js
+++ client/app/controllers/IndexCtrl.js
@@ -8,9 +8,35 @@ controllers.controller('IndexController', ['$scope',
 	function($scope){
 
 	var vm = this;
+
+	vm.message = {};
+	vm.sendSuccess = false;
+	vm.sendError = false;
 	
 	vm.init = function(){
 		//nothing yet
+	};
+
+	vm.scrollToId = function(id){
+		//scroll to the element with jquery
+	    $('html, body').animate({
+	        scrollTop: $("#" + id).offset().top
+	    }, 2000);
+	};
+
+	/**
+	* Send the message to the backend, where it can be email to me
+	*/
+	vm.submit = function(message){
+
+		MessageService.save(message, function(response){
+			vm.sendSuccess = false;
+			vm.sendError   = true;
+		}, function(error){
+			vm.sendSuccess = false;
+			vm.sendError   = true;
+		});
+
 	};
 
 	vm.init();
--- client/app/directives/leftColumn.js
+++ client/app/directives/leftColumn.js
@@ -9,6 +9,9 @@ directives.directive('leftColumn', function(){
 	return {
 		restrict: 'E',
 		replace: true,
+		scope:{
+            data: '=info'
+        },
 		templateUrl: '/app/views/templates/leftColumn.html',
 		link: function(scope, element, attrs) {
 
