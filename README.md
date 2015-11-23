dff9a5cceec41eefd8986d57fcc473527acbdf04
Daniel Jamrozik <djamro2@illinois.edu>
2015-11-23 14:03:35 -0600

    new directory to match new site

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # danjamrozik.me
+
+# Personal blog site to write about web dev and machine learning
+# All coding examples from blog are in /blog-examples. Old site built from from scratch is in /old-site
--- client/app/services/resources.js
+++
@@ -1,9 +0,0 @@
-/* global angular */
-
-var factories = factories || angular.module('PortfolioWebsite.factories', []);
-
-factories.factory('MessageService', function($resource){
-	return $resource('/api/message', {}, {
-		update: {method: 'PUT'}
-	});
-});
