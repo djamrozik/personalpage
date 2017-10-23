d1894d0d8f52a74249df992fe85abc6bf43d4d17
Daniel Jamrozik <djamro2@illinois.edu>
2017-10-22 19:24:06 -0500

    update folder structure, delete unneeded files

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Personal blog site to write about web dev and machine learning
 
-All coding examples from blog are in /blog-examples. Old site built from from scratch is in /old-site
+All coding examples from blog are in /blog-examples.
---
+++ blog-examples/d3_simple_line_chart/line_graph.js
@@ -0,0 +1,54 @@
+var data = [{
+    "sale": "202",
+    "year": "2000"
+}, {
+    "sale": "215",
+    "year": "2001"
+}, {
+    "sale": "179",
+    "year": "2002"
+}, {
+    "sale": "199",
+    "year": "2003"
+}, {
+    "sale": "176",
+    "year": "2010"
+}];
+
+var vis = d3.select("#visualisation");
+var WIDTH = 1000;
+var HEIGHT = 500;
+var MARGINS = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 50
+};
+
+var xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000,2010]);
+var yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134,215]);
+
+var xAxis = d3.svg.axis().scale(xScale);
+var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+vis.append("svg:g")
+    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+    .call(xAxis);
+
+vis.append("svg:g")
+    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+    .call(yAxis);
+
+var lineGen = d3.svg.line()
+    .x(function(d) {
+        return xScale(d.year)
+    })
+    .y(function(d) {
+        return yScale(d.sale)
+    });
+
+vis.append("svg:path")
+    .attr("d", lineGen(data))
+    .attr("stroke", "green")
+    .attr("stroke-width", 2)
+    .attr("fill", "none");
