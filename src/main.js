2ac99ee4aa0cdb2ec36eb3f59808bfcbb183afcb
Daniel Jamrozik <djamro2@illinois.edu>
2017-12-27 13:10:52 -0600

    latest update

--- src/main.js
+++ src/main.js
@@ -2,17 +2,28 @@
 // global imports
 import Vue from "vue";
 import VeeValidate from "vee-validate";
+import VueRouter from "vue-router";
 
 // local imports
 import ContactForm from "./components/ContactForm.vue";
 import CollapseNav from "./components/CollapseNav.vue";
 import LeftColumn from "./components/LeftColumn.vue";
+import LoginBox from "./components/LoginBox.vue"
 
 // setup plugins
 Vue.use(VeeValidate);
+Vue.use(VueRouter);
 
 // config
 Vue.config.productionTip = false
+
+// routes and router
+
+const routes = [
+  { path: "/index.html", }
+];
+
+const router = new VueRouter({routes});
 
 // root app
 new Vue({
@@ -20,6 +31,7 @@ new Vue({
   components: {
     "collapse-nav": CollapseNav,
     "contact-form": ContactForm,
-    "left-column": LeftColumn
+    "left-column": LeftColumn,
+    "login-box": LoginBox
   }
 });
